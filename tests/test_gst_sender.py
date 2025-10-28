f574158ca14a9c7e37edb6a8e8aaef5ebc0df1f3
cthsu <chuntsehsu@gmail.com>
2025-10-28 09:40:53 +0800

    feat: new initialize for code improved

--- tests/test_gst_sender.py
+++ tests/test_gst_sender.py
@@ -1,6 +1,6 @@
 import pytest
 
-from gst_realsense_launch.startup.gst_sender import DeviceInfo, Mode, find_best_mode
+from gst_realsense_launch.startup.rs_detect import DeviceInfo, Mode, RealSenseDetector
 
 
 def test_sender_placeholder():
@@ -22,7 +22,7 @@ def sample_device_info():
 def test_find_best_mode_exact_match(sample_device_info):
     """Test whether it can be found correctly when there is an exact matching resolution."""
     target_size = (640, 480)
-    mode = find_best_mode(sample_device_info, target_size, "color")
+    mode = RealSenseDetector.find_best_mode(sample_device_info, target_size, "color")
     assert mode is not None
     assert mode.size == target_size
     assert mode.fourcc == "YUYV"
@@ -31,7 +31,7 @@ def test_find_best_mode_exact_match(sample_device_info):
 def test_find_best_mode_closest_match(sample_device_info):
     """Tests whether the closest resolution can be found when there is no exact matching resolution."""
     target_size = (800, 600)
-    mode = find_best_mode(sample_device_info, target_size, "color")
+    mode = RealSenseDetector.find_best_mode(sample_device_info, target_size, "color")
     assert mode is not None
     assert mode.size == (640, 480)
 
@@ -39,5 +39,5 @@ def test_find_best_mode_closest_match(sample_device_info):
 def test_find_best_mode_no_match(sample_device_info):
     """Tests whether None is returned when there is no pattern matching the streaming type."""
     target_size = (640, 480)
-    mode = find_best_mode(sample_device_info, target_size, "non_existent_type")
+    mode = RealSenseDetector.find_best_mode(sample_device_info, target_size, "non_existent_type")
     assert mode is None
